a8fae3f1ef92bbb550b92a09522a51fc174266f4
mvkrsna <krsna.genai@gmail.com>
2025-11-01 21:52:24 -0500

    Feat: Committing the initial files for Kafka tutorials
    Feat: Committing the initial files for Kafka tutorials

---
+++ src/test/java/com/techbud/kafkatutorials/kafka_tutorials/KafkaTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbud.kafkatutorials.kafka_tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
